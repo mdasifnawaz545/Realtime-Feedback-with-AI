92b5ec9d9269826d6a2f3b91f1809537e12c5a3a
mdasifnawaz545 <mdasifnawaz545@gmail.com>
2025-02-22 11:49:19 +0530

    Add Note on the verification page for the restriction of Re-send Mail service provider

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -214,20 +214,22 @@ function Verify({ params: { username } }: Props) {
                                 ) : "Verify"
                             }
                         </Button>
+
+                        <br />
+
+                        <div>
+                            <p className="text-sm text-red-500 mt-2 text-justify"><b>NOTE: </b>
+                                Due to email provider restrictions, OTP emails can only be sent to a specific email address. If you haven't received your OTP, please contact us at{" "}
+                                <a href="mailto:mdasifnawaz545@gmail.com" className="text-blue-500 underline">
+                                    mdasifnawaz545@gmail.com
+                                </a>{" "}
+                                so we can provide existing credentials for authentication. We apologize for the inconvenience and appreciate your patience.
+                            </p>
+                        </div>
+
                     </form>
                 </Form>
-               <div>
 
-
-<p className="text-sm text-red-500 mt-2">
-  Due to email provider restrictions, OTP emails can only be sent to a specific email address. If you haven't received your OTP, please contact us at{" "}
-  <a href="mailto:mdasifnawaz545@gmail.com" className="text-blue-500 underline">
-    mdasifnawaz545@gmail.com
-  </a>{" "}
-  so we can provide existing credentials for authentication. We apologize for the inconvenience and appreciate your patience.
-</p>
-
-               </div>
             </div>
 
         </div>
